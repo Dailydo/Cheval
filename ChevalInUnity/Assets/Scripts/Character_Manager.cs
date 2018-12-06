a38a79517440cd486fa7faa1b6c90ee794b0355d
DESKTOP-4VP0SSO\Dailydo <dailydo_31@hotmail.fr>
2018-12-07 00:59:29 +0100

    Clothing system v1

---
+++ ChevalInUnity/Assets/Scripts/Character_Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character_Manager : MonoBehaviour {
+
+    public Inventory _inventory;
+
+    public GameObject _head;
+    public GameObject _chest;
+    public GameObject _legs;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update (){
+
+    }
+
+    public void AssignItem (GameObject button)
+    {
+        button.GetComponent<ButtonItemInfo>()._bodyPart.GetComponent<SpriteRenderer>().sprite = button.GetComponent<ButtonItemInfo>()._ItemSprite;
+    }
+}
---
+++ ChevalInUnity/Assets/Scripts/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Inventory", order = 1)]
+public class Inventory : ScriptableObject
+{
+    public List<Item> _itemList = new List<Item>();
+}
+
+[System.Serializable]
+public struct Item
+{
+    public string _itemName;
+    public Sprite _itemImage;
+}
